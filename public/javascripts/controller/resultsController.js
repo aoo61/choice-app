9d0725ed593edccded9ede37e87817258aa04a80
aoo61 <a.osman.ozoglu@gmail.com>
2019-12-19 13:57:18 +0300

    results page edit

--- public/javascripts/controller/resultsController.js
+++ public/javascripts/controller/resultsController.js
@@ -5,6 +5,7 @@ let choiceD = null;
 window.app = angular.module('Results', []);
 app.controller('resultsController', function ($scope, $http) {
     $scope.vote = null;
+    $scope.invalidVote = null;
     $scope.initialSearchMade = false;
     $http.get(`/blockchain/result`)
         .then(function (response) {
@@ -12,7 +13,8 @@ app.controller('resultsController', function ($scope, $http) {
             choiceB = parseInt(response.data.choiceB);
             choiceC = parseInt(response.data.choiceC);
             choiceD = parseInt(response.data.choiceD);
-            $scope.vote = response.data.vote;
+            $scope.vote = parseInt(response.data.vote);
+            $scope.invalidVote = $scope.vote - (choiceA + choiceB + choiceC + choiceD);
         });
 });
 
@@ -23,13 +25,13 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
     let data = google.visualization.arrayToDataTable([
         ['Oy', 'Oy Sayısı'],
-        ['A', choiceA],
-        ['B', choiceB],
-        ['C', choiceC],
-        ['D', choiceD]
+        ['A Partisi', choiceA],
+        ['B Partisi', choiceB],
+        ['C Partisi', choiceC],
+        ['D Partisi', choiceD]
     ]);
     // Optional; add a title and set the width and height of the chart
-    var options = {'title': 'Genel Seçim Sonuçları', 'width': 600, 'height': 600};
+    var options = {'title': 'Genel Seçim Sonuçları', 'width': 600, 'height': 600, is3D:true};
     // Display the chart inside the <div> element with id="piechart"
     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
     chart.draw(data, options);
